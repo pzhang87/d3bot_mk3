77751603761be15d799638fbf919b96d2ef71e1c
Peter Zhang <pzhang87@gmail.com>
2018-03-30 07:55:21 -0400

    add touhouwiki

--- commands.js
+++ commands.js
@@ -2,11 +2,12 @@ const Search = require('./search.js');
 
 module.exports = {
   'killd3bot': () => { return 'no u' },
+  '2hu': Search.find,
   'im@s': Search.find,
   'imas': Search.find,
   'feh': Search.find,
   'kc': Search.find,
   '+dkpl': () => { return "that command is unimplemented. you currently have 0 dkpls" },
   '-dkpl': () => { return "that command is unimplemented. you currently have 0 dkpls" },
-  'default': () => { return 'unrecognized command'}
+  'default': () => { return 'unrecognized command' }
 }
