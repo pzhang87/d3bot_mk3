1342c213e59c4302023a415fd5f619c4f903dc78
Peter Zhang <pzhang87@gmail.com>
2018-04-05 20:08:58 -0400

    meh

--- scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
- export NODE_ENV=$DEPLOYMENT_GROUP_NAME
-fi
-
-cd /ec2-user/home/d3bot_mk3
-npm start
--- scripts/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd ~/d3bot_mk3
-pm2 stop d3bot || true
