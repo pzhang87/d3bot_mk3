13cbc7e97912b2f4510c6bab57f8ab595f26eab8
Peter Zhang <pzhang87@gmail.com>
2018-04-05 21:11:23 -0400

    maybe this will finally do it

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,5 +3,4 @@
 #  export NODE_ENV=$DEPLOYMENT_GROUP_NAME
 # fi
 #
-cd /ec2-user/home/d3bot_mk3
-pm2 startOrRestart config/ecosystem.json --env production
+pm2 startOrRestart /ec2-user/home/d3bot_mk3/config/ecosystem.json --env production
